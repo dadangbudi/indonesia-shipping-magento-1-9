77ecec0d6daf76836dac5c836c089aebd8674d37
zemna <zemna@zemna.net>
2017-04-03 16:32:24 +0700

    Initial Import

---
+++ app/code/local/Ansyori/Aongkir/Model/System/Config/Source/Kurir.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Indonesia Shipping Carriers
+ * @copyright   Copyright (c) 2015 Ansyori B.
+ * @email		ansyori@gmail.com / ansyori@kemanaservices.com
+ * @build_date  March 2015
+ * @license     http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ */
+class Ansyori_Aongkir_Model_System_Config_Source_Kurir
+{
+  public function toOptionArray()
+  {
+    return array(
+      array('value' => 'JNE', 'label'=>Mage::helper('adminhtml')->__('JNE')),
+      array('value' => 'POS', 'label'=>Mage::helper('adminhtml')->__('POS')),
+      array('value' => 'TIKI', 'label'=>Mage::helper('adminhtml')->__('TIKI')),
+    );
+  }
+}
